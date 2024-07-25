5a9ca4cb350ebe9d899d20640a91737f8136d93a
Joel-Steve NIKENOUEBA <leducnikos@gmail.com>
2024-07-25 18:32:00 +0100

    Fixs

--- app/main/events/consumers/subscriber.py
+++ app/main/events/consumers/subscriber.py
@@ -5,11 +5,10 @@ import redis
 
 from app.main.core.config import Config
 from app.main.events.consumers.notification import NotificationConsume
-from app.main.events.topics.consume_topics import ConsumeTopics
 
 db = redis.Redis(
-    host=Config.SUBSCRIBER_REDIS_HOST,
-    port=Config.SUBSCRIBER_REDIS_PORT,
+    host=Config.REDIS_HOST,
+    port=Config.REDIS_PORT,
     decode_responses=True
 )
 
@@ -17,15 +16,14 @@ consumer = db.pubsub()
 
 
 def process_messages():
-    consumer.subscribe(Config.SUBSCRIBER_REDIS_CHANNEL)
+    consumer.subscribe(Config.REDIS_DB)
     for message in consumer.listen():
         print(message)
         if message.get('type') == 'message':
             data = json.loads(message.get('data'))
             message_type = data.get('type')
             data = data.get('data')
-            if message_type == ConsumeTopics.EVENT_NEW_NOTIFICATION.value:
-                return NotificationConsume.consume_general(data)
+            return NotificationConsume.consume_general(data)
 
             print(f"messages ---> {message_type} -- {data}")
         time.sleep(0.5)
