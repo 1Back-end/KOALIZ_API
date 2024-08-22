bf4ab0ee58f7235f1d87dd7dd095c3cdd4ec3a07
Severin Fernando Tanga Mvondo <jarodak47@gmail.com>
2024-08-22 17:45:27 +0100

    update

--- app/main/schemas/preregistration.py
+++ app/main/schemas/preregistration.py
@@ -6,7 +6,7 @@ from datetime import datetime, time, date
 
 from app.main import models
 from app.main.core.i18n import __
-from app.main.models.children import AdditionalCare, CareType, Cleanliness, MealQuality, NapQuality, Route, StoolType,PipiStoolTypeEnum,PipiStoolTypeEnum
+from app.main.models.children import AdditionalCare, CareType, Cleanliness, MealQuality, MealTypeEnum, NapQuality, Route, StoolType,PipiStoolTypeEnum,PipiStoolTypeEnum
 from app.main.schemas import DataList, NurseryMini
 # from app.main.schemas.activity import ActivityResponse
 from app.main.schemas.attendance import AttendanceMini
@@ -407,6 +407,7 @@ class MealSlim(BaseModel):
     bottle_milk_ml: Optional[int] = None
     breastfeeding_duration_minutes: Optional[int] = None
     meal_quality: Optional[MealQuality] = None
+    meal_type:Optional[MealTypeEnum] = None
     observation: Optional[str] = None    
     # nursery: Optional[NurserySlim]=None
     # added_by: Optional[EmployeBase]=None
